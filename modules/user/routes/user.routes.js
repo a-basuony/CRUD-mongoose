27c705454486808432351977cb15542a04e07fef
Ahmed Basuony <A.Basuony.social@gmail.com>
2024-02-28 08:15:07 +0200

    Committing changes before rebase

--- modules/user/routes/user.routes.js
+++ modules/user/routes/user.routes.js
@@ -18,24 +18,3 @@ module.exports = router;
 
 // PUT => Used to completely update or replace an existing resource with the request payload.
 // PATCH => Used to partially (some of data not completely) update or replace a resource with the request payload. It is used to set only specific fields of a resource.
-
-const {
-  getAllUsers,
-  addNewUser,
-  getUser,
-  deleteUser,
-  updateUser,
-} = require("../controller/user.controller");
-
-const router = require("express").Router();
-
-router.get("/users", getAllUsers);
-router.get("/getUser/:id", getUser);
-router.post("/addNewUser", addNewUser);
-router.delete("/delete/:id", deleteUser);
-router.patch("/updateUser/:id", updateUser);
-
-module.exports = router;
-
-// PUT => Used to completely update or replace an existing resource with the request payload.
-// PATCH => Used to partially (some of data not completely) update or replace a resource with the request payload. It is used to set only specific fields of a resource.
